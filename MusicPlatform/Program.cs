93dfb108fc1b7b89956047978b227764b96825f6
VasilenK838 <vasilenkondakov@gmail.com>
2025-07-03 19:29:05 +0300

    refactor: add types to properties in Program.cs and add authentication middleware

--- MusicPlatform/Program.cs
+++ MusicPlatform/Program.cs
@@ -7,10 +7,11 @@ namespace MusicPlatform.Web
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            // ---------------------------------------------------------------------------------------------------------//
+            // Services to container
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -19,7 +20,7 @@ namespace MusicPlatform.Web
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+            WebApplication app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -38,6 +39,7 @@ namespace MusicPlatform.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
