4441cfe2f3d44fc07fa1d8f540811f89bbee1bae
VasilenK838 <vasilenkondakov@gmail.com>
2025-07-10 21:49:36 +0300

    add: Profile, Playlist and Genre controllers

---
+++ MusicPlatform/Controllers/PlaylistController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicPlatform.Web.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
