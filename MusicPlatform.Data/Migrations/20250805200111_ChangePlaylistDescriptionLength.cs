225b8a562b8515245e0ba4ddc087bf5f2a366a1d
VasilenK838 <vasilenkondakov@gmail.com>
2025-08-05 23:03:57 +0300

    refactor: Playlist entity model description length from 500 to 50 chars

---
+++ MusicPlatform.Data/Migrations/20250805200111_ChangePlaylistDescriptionLength.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicPlatform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePlaylistDescriptionLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Playlists",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                comment: "An optional description for the playlist.",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true,
+                oldComment: "An optional description for the playlist.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Playlists",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                comment: "An optional description for the playlist.",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true,
+                oldComment: "An optional description for the playlist.");
+        }
+    }
+}
